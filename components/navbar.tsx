d73bf466ada5cfd93ffee47a7c8595ccea04734c
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-09-07 10:55:52 +0530

    navigation

---
+++ components/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import Header from "./header";
+
+const links = [
+  {
+    href: "/",
+    label: "home",
+  },
+  {
+    href: "/blog",
+    label: "blog",
+  },
+  {
+    href: "/work",
+    label: "work",
+  },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav>
+      <ul className="flex items-center gap-3 py-10 text-lg">
+        {links.map(({ href, label }) => {
+          return (
+            <Link href={href} key={href}>
+              <li
+                className={`
+                ${
+                  pathname === href
+                    ? "text-foreground font-bold"
+                    : "text-gray-500 hover:text-foreground transition-colors duration-200"
+                }
+                `}
+              >
+                {label}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
